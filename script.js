d5bf3f91f2d86113d376e0d57adf1e768704264f
timble <timblemichael20@gmail.com>
2022-05-15 19:44:47 +1000

    adding further functionalaties

--- script.js
+++ script.js
@@ -33,3 +33,26 @@ $(document).ready(function(){
     })
     
 })
+
+
+// Declaring variable for the 'content' and the reseptive 'tab'
+
+const tabs = document.querySelectorAll('[data-target]'), tabContents = document.querySelectorAll('[data-content]')
+
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification-active')
+        })
+
+        target.classList.add('qualification-active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification-active')
+        })
+        tab.classList.add('qualification-active')
+    })
+})
